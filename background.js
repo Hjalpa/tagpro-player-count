dec378019bb48b6eaed4730db1cbfc4512b10147
Jeff Everett <everett.jeff.w@gmail.com>
2016-05-17 15:58:11 -0600

    Extension now saves server setting

--- background.js
+++ background.js
@@ -7,108 +7,14 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     });
 });
 
-
 function changeTrackedServer(server) {
     trackedServer = server;
+
+    chrome.storage.sync.set({server: trackedServer});
 
     // update immediately so player does not have to wait
     updatePlayerCount();
 }
-
-// register radio buttons for all servers
-chrome.contextMenus.create({
-    title: "Choose server to track",
-    id: "server_choice",
-    contexts: ["browser_action"]
-});
-
-chrome.contextMenus.create({
-    title: "Track Centra",
-    parentId: "server_choice",
-    type: "radio",
-    checked: false,
-    contexts: ["browser_action"],
-    onclick: function() {
-        changeTrackedServer("centra");
-    }
-});
-
-chrome.contextMenus.create({
-    title: "Track Chord",
-    parentId: "server_choice",
-    type: "radio",
-    checked: false,
-    contexts: ["browser_action"],
-    onclick: function() {
-        changeTrackedServer("chord");
-    }
-});
-
-chrome.contextMenus.create({
-    title: "Track Diameter",
-    parentId: "server_choice",
-    type: "radio",
-    checked: false,
-    contexts: ["browser_action"],
-    onclick: function() {
-        changeTrackedServer("diameter");
-    }
-});
-
-chrome.contextMenus.create({
-    title: "Track Orbit",
-    parentId: "server_choice",
-    type: "radio",
-    checked: false,
-    contexts: ["browser_action"],
-    onclick: function() {
-        changeTrackedServer("orbit");
-    }
-});
-
-chrome.contextMenus.create({
-    title: "Track Origin",
-    parentId: "server_choice",
-    type: "radio",
-    checked: false,
-    contexts: ["browser_action"],
-    onclick: function() {
-        changeTrackedServer("origin");
-    }
-});
-
-chrome.contextMenus.create({
-    title: "Track Pi",
-    parentId: "server_choice",
-    type: "radio",
-    checked: false,
-    contexts: ["browser_action"],
-    onclick: function() {
-        changeTrackedServer("pi");
-    }
-});
-
-chrome.contextMenus.create({
-    title: "Track Radius",
-    parentId: "server_choice",
-    type: "radio",
-    checked: false,
-    contexts: ["browser_action"],
-    onclick: function() {
-        changeTrackedServer("radius");
-    }
-});
-
-chrome.contextMenus.create({
-    title: "Track Sphere",
-    parentId: "server_choice",
-    type: "radio",
-    checked: true,
-    contexts: ["browser_action"],
-    onclick: function() {
-        changeTrackedServer("sphere");
-    }
-});
 
 function updatePlayerCount() {
     // check player count
@@ -147,10 +53,110 @@ function updatePlayerCount() {
     });
 }
 
-(function() {
-    // check player count initially
-    updatePlayerCount();
+chrome.storage.sync.get("server", function(data) {
+    if (data.server)
+        trackedServer = data.server;
 
-    // check again in 5 seconds
-    setTimeout(arguments.callee, 5000);
-})();
+    // register radio buttons for all servers
+    chrome.contextMenus.create({
+        title: "Choose server to track",
+        id: "server_choice",
+        contexts: ["browser_action"]
+    });
+
+    chrome.contextMenus.create({
+        title: "Track Centra",
+        parentId: "server_choice",
+        type: "radio",
+        checked: false,
+        contexts: ["browser_action"],
+        onclick: function() {
+            changeTrackedServer("centra");
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Track Chord",
+        parentId: "server_choice",
+        type: "radio",
+        checked: false,
+        contexts: ["browser_action"],
+        onclick: function() {
+            changeTrackedServer("chord");
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Track Diameter",
+        parentId: "server_choice",
+        type: "radio",
+        checked: false,
+        contexts: ["browser_action"],
+        onclick: function() {
+            changeTrackedServer("diameter");
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Track Orbit",
+        parentId: "server_choice",
+        type: "radio",
+        checked: false,
+        contexts: ["browser_action"],
+        onclick: function() {
+            changeTrackedServer("orbit");
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Track Origin",
+        parentId: "server_choice",
+        type: "radio",
+        checked: false,
+        contexts: ["browser_action"],
+        onclick: function() {
+            changeTrackedServer("origin");
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Track Pi",
+        parentId: "server_choice",
+        type: "radio",
+        checked: false,
+        contexts: ["browser_action"],
+        onclick: function() {
+            changeTrackedServer("pi");
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Track Radius",
+        parentId: "server_choice",
+        type: "radio",
+        checked: false,
+        contexts: ["browser_action"],
+        onclick: function() {
+            changeTrackedServer("radius");
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Track Sphere",
+        parentId: "server_choice",
+        type: "radio",
+        checked: true,
+        contexts: ["browser_action"],
+        onclick: function() {
+            changeTrackedServer("sphere");
+        }
+    });
+
+    (function() {
+        // check player count initially
+        updatePlayerCount();
+
+        // check again in 5 seconds
+        setTimeout(arguments.callee, 5000);
+    })();
+});
